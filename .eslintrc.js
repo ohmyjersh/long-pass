dbf26764150309bb4cf8a913a0ca65352b92ae5d
Christian Danielsen <ckdanielsen@gmail.com>
2017-08-16 15:31:13 -0700

    clean up some scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,14 @@ module.exports = {
   "extends": ["standard", "plugin:react/recommended"],
   "env": {
     "browser": true,
-    "es6": true
+    "es6": true,
+    "node": true,
+    "commonjs": true
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
   "plugins": [
     "react"
