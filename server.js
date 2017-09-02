dc331cf1005fcb4d5dcb6a34b996d6946f9948fd
Josh Jarmain <jjarmain@gmail.com>
2017-09-01 22:05:19 -0700

    add server bacccck

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+const { join } = require('path')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 3001
+const assets = join(__dirname, 'build')
+
+// Serve static files from ./build
+app.use(express.static(assets))
+
+// Top level route
+app.get('/', (request, response) => {
+  response.sendFile(join(__dirname, '/index.html'))
+})
+
+// Server listener
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log('Something bad happened', err)
+  }
+  console.log(`Huzzah! Server is listening at http://localhost:${PORT}`)
+})
